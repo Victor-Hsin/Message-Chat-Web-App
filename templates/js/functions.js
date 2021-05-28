32db8869bef6469693e69a684e9cbcb8f587edf5
Victor-Hsin <victor_hsin@berkeley.edu>
2021-05-28 06:22:43 -0400

    added login page

---
+++ templates/js/functions.js
@@ -0,0 +1,14 @@
+const userName = document.querySelector("#userName")
+const password = document.querySelector("#password")
+// const loginBtn = document.querySelector("#loginBtn");
+// const registerBtn = document.querySelector("#registerBtn");
+
+function userLogin(){
+    console.log("logged in")
+    // to-do: fetch flask API to check the username and password of the user
+}
+
+function signUpNewAccount(){
+    // to-do: fetch flask API to register and login the user
+    console.log("logged out")
+}
