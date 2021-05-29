b330cf0dbd2652df1a4850d4c417d289021f22d7
Victor-Hsin <victor_hsin@berkeley.edu>
2021-05-29 12:18:24 -0400

    added message windows, input area, and send button

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from crypt import methods
 from flask import Flask, request, session, redirect, url_for, render_template
 from flaskext.mysql import MySQL
 import pymysql 
-import re 
 
 app = Flask(__name__)
 app.secret_key = "evou\x878\xa0\xe0FF\x8e~\x92\xf1xvCE\xe0\x15\x81\x17=\xf1"
@@ -52,7 +50,7 @@ def signUp():
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
 
-    # provide an error message if things go wrong
+    # output an message for whether things go wrong
     msg = ''
     if request.method == "POST" and "username" in request.form and 'password' in request.form:
         username = request.form['username']
@@ -71,7 +69,6 @@ def signUp():
             msg = "Sign up sucessfully! Please enter the username and password again to log in."
         else:
             msg = "This username was already taken. Please try another username."
-
     return render_template('login.html', msg=msg)
 
 
--- static/js/messagePage.js
+++ static/js/messagePage.js
@@ -2,7 +2,6 @@ const userName = document.querySelector("#username")
 const password = document.querySelector("#password")
 const loginBtn = document.querySelector("#loginBtn")
 const registerBtn = document.querySelector("#registerBtn")
-const form = document.querySelector("#userInfo-form")
 const msg = document.querySelector("#msg")
 const msgText = msg.innerHTML
 
@@ -11,16 +10,6 @@ if (msgText == "Sign up sucessfully! Please enter the username and password agai
     msg.setAttribute("style", "color: green;")
 } else {
     msg.setAttribute("style", "color: red;")
-}
-// if (msgText === "Incorrect username or password. Please try again.") {
-//     msg.setAttribute("style", "color: red;")
-// } else if (msgText === "Sign up sucessfully! Please click the LOGIN button to log in.") {
-//     msg.setAttribute("style", "color: green;")
-// }
-
-function signUpNewAccount(){
-    // to-do: fetch flask API to register and login the user
-    console.log("logged out")
 }
 
 
