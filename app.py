9b89df4c8e3db97ed4de79f2b25087269abecea4
Victor-Hsin <victor_hsin@berkeley.edu>
2021-06-02 11:26:00 -0400

    dockerize application

--- app.py
+++ app.py
@@ -9,10 +9,46 @@ app.secret_key = "evou\x878\xa0\xe0FF\x8e~\x92\xf1xvCE\xe0\x15\x81\x17=\xf1"
 mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'messageApp'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
 mysql.init_app(app)
+
+def createTables():
+    # connect to database
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    try:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS user (
+                user_id int(20) NOT NULL AUTO_INCREMENT,
+                username varchar(45) NOT NULL,
+                password varchar(45) NOT NULL,
+                PRIMARY KEY (user_id)
+            )
+        """)  
+        conn.commit()
+    except: 
+        print("**************Fail to create user table!!!***************")  
+    
+
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    try:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS message (
+                message_id int(20) NOT NULL AUTO_INCREMENT,
+                user_id int(20) NOT NULL,
+                creation_date datetime NOT NULL,
+                message text,
+                PRIMARY KEY (message_id),
+                FOREIGN KEY (user_id) REFERENCES user(user_id)
+            )
+        """)    
+        conn.commit()
+    except: 
+        print("**************Fail to create message table!!!***************") 
+    
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -113,4 +149,5 @@ def messages():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    createTables()
+    app.run(debug=True, host='0.0.0.0', port=5000)
